a92626455e042092a7266c65d77bac9ef99cef7c
vvh <ns@ywa.red>
2024-12-20 18:30:04 +0300

    chore: adjusted default rgb color (#ff00ff) + set default brightness to 40%

--- keyboards/keychron/v6_max/ansi_encoder/keymaps/vvh413/config.h
+++ keyboards/keychron/v6_max/ansi_encoder/keymaps/vvh413/config.h
@@ -1,9 +1,10 @@
 #ifdef RGB_MATRIX_ENABLE
 
-/* solid #e100ff */
+/* solid #ff00ff */
 #    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
-#    define RGB_MATRIX_DEFAULT_HUE 208
+#    define RGB_MATRIX_DEFAULT_HUE 213
 #    define RGB_MATRIX_DEFAULT_SAT 255
-#    define RGB_MATRIX_DEFAULT_VAL 255
+/* default brightness = 102 = 6 * 17 = 40% */
+#    define RGB_MATRIX_DEFAULT_VAL 102
 
 #endif
